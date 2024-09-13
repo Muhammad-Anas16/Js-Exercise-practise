b4c8dab26070641349b36f000bacd21f0930896c
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-13 06:46:45 -0700

    first commit

---
+++ chapter # 01/app.js
@@ -0,0 +1,19 @@
+// Q no 1
+
+alert("hello");
+
+// Q no 2
+
+alert("hello world \nhappy coding");
+
+// Q no 3
+
+alert("I'm using Javascript");
+
+// Q no 4
+
+alert("welcome to Js land");
+
+// Q no 5
+
+alert("hello .................................")
---
+++ chapter # 2/app.js
@@ -0,0 +1,18 @@
+// Q no # 1
+
+var userName = "Ali";
+var myName = "Anas";
+var massege = "hello world";
+
+alert(massege);
+
+var studentName = myName;
+var age = 23;
+var grade = "A";
+var collage = "xyz collage";
+
+alert("student Name: " + studentName);
+alert("age: " + age);
+alert("grade: " + grade);
+alert("collage: " + school);
+

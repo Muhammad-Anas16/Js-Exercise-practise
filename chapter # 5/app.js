f153be7c9f8e2378417d460b576713237c815755
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-14 12:09:22 -0700

    complete ch # 5

---
+++ chapter # 5/app.js
@@ -0,0 +1,45 @@
+// ***************************** Q no # 1*********************************
+
+var num1 = 5;
+var num2 = 3;
+
+var num = ("The Sum of Num1 & Num2 is : ") + ( num1 + num2);
+
+console.log(num);
+
+// ***************************** Q no # 2*********************************
+
+var num = ("The Subtraction of Num1 & Num2 is : ") + ( num1 - num2 );
+
+console.log(num);
+
+// Q no # 2 (  B  )
+
+var num = ("The Multiplication of Num1 & Num2 is : ") + ( num1 * num2 );
+
+console.log(num);
+
+// Q no # 2 (  C  )
+
+var num = ("The Division of Num1 & Num2 is : ") + ( num1 / num2 );
+
+console.log(num);
+
+
+// ***************************** Q no # 3*********************************
+
+var value =
+
+console.log(value);
+
+value = 5;
+
+console.log("Initial Value : "+ value);
+
+value = value + 2;
+
+console.log("increament value is : " + value);
+
+value = value - 7;
+
+console.log("Value After Decrement is : " + value);

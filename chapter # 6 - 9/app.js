82efa197608bb529a0d55440369323d1f2957847
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-15 08:24:53 -0700

    15-sep-2024

---
+++ chapter # 6 - 9/app.js
@@ -0,0 +1,51 @@
+// Q no 1
+console.log("****** Q no : 1 ******")
+
+// let a = 10;
+
+// console.log("The value of a is : " + a);
+
+// a = ++a;
+
+// console.log("The value of ++a is : " + a);
+
+// a = a ++;
+
+// console.log("The value of a++ is : " + a);
+
+// a = --a ;
+
+// console.log("The value of --a is : " + a);
+
+// //  Q no 2
+
+// var c = 2, b = 1; 
+// var result = --c - --b + ++b + b--;
+
+// console.log(result);
+
+// Q no 2
+console.log("****** Q no : 2 ******")
+
+// var userName = prompt("Enter Your Name");
+
+// var value = userName;
+
+// if(value === userName){
+//     document.write("hello :" + userName);
+// }
+
+// Q no 3
+console.log("****** Q no : 3 ******")
+
+// var num = prompt("Enter a Number");
+
+// console.log("The value of {var num} :" + num);
+
+// var multi = num * 5;
+
+// console.log("The Value of {var mullti} by multiply by 5 : " + multi);
+
+// Q no 4
+console.log("****** Q no : 4 ******")
+

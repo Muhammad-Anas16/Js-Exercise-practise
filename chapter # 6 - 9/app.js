089e873e36da6f032be987e6f59300022a03ed75
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-17 05:22:56 -0700

    compelet

--- chapter # 6 - 9/app.js
+++ chapter # 6 - 9/app.js
@@ -49,3 +49,40 @@ console.log("****** Q no : 3 ******")
 // Q no 4
 console.log("****** Q no : 4 ******")
 
+// ********************** Subject Name ********************** 
+
+var Subject1 = prompt("Enter Subject Name", "Computer");
+console.log("Console of subject-1 : ",Subject1);
+document.getElementById("subject-1").innerText = Subject1;
+
+var Subject2 = prompt("Enter Subject Name", "Mathametics");
+console.log("Console of subject-2 : ",Subject1);
+document.getElementById("subject-2").innerText = Subject2;
+
+var Subject3 = prompt("Enter Subject Name", "Geography");
+console.log("Console of subject-3 : ",Subject1);
+document.getElementById("subject-3").innerText = Subject3;
+
+// ********************** Marks Obtained *********************
+
+var markObtain1 = + prompt("Enter Obtained Marks");
+document.getElementById("marks-obt-1").innerText = markObtain1;
+
+var markObtain2 = + prompt("Enter Obtained Marks");
+document.getElementById("marks-obt-2").innerText = markObtain2;
+
+var markObtain3 = + prompt("Enter Obtained Marks");
+document.getElementById("marks-obt-3").innerText = markObtain3;
+
+
+// ********************** Total Marks ********************** 
+
+var total = markObtain1 + markObtain2 + markObtain3;
+console.log(total)
+document.getElementById("total-marks").innerText = total;
+
+// ********************** Percentage ***********************
+
+var countPercentage = total / 300 *100;
+console.log(countPercentage);
+document.getElementById("percentage").innerText = countPercentage;

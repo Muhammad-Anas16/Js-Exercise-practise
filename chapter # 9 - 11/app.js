4f18a225cf09ef038fb78b053d90e153f84ce7c3
muhammad-anas16 <anas.hanf38@gmail.com>
2024-09-22 07:19:05 -0700

    9 - 11 completed

---
+++ chapter # 9 - 11/app.js
@@ -0,0 +1,69 @@
+
+console.log("Q no # 1")
+// Q no # 01
+
+// var cityName = prompt("Enter your City Name","karachi");
+
+// if(cityName === "karachi"){
+//     document.getElementById("cityName").innerText = "wellcome to the city of light " + cityName;
+
+// }else{
+//     document.getElementById("cityName").innerText = "You'r not from Karachi";
+// }
+
+console.log("Q no # 2")
+// Q no # 02
+
+// var gender = prompt("Enter Your Gender");
+
+// if(gender === "male"){
+//     document.getElementById("Gender").innerText = "hello Sir"
+// }else if(gender === "female"){
+//     document.getElementById("Gender").innerText = "hello Madam"
+// }else{
+//     document.getElementById("Gender").innerText = "Kindly Enter Your Gender !"
+// }
+
+console.log("Q no # 3")
+// Q no # 04
+
+// console.log("Q no # 3 -- A")
+
+// var color1 = prompt("Enter Traffic lights");
+// document.getElementById("color1").innerText = color1;
+
+// if(color1 === "red"){
+//     document.getElementById("means1").innerText = "Must Stop";
+
+// }else{
+// document.getElementById("means1").innerText = "Undefine";
+// }
+
+// console.log("Q no # 3 -- B")
+
+// var color2 = prompt("Enter Traffic lights");
+// document.getElementById("color2").innerText = color2;
+
+// if(color2 === "yellow"){
+//     document.getElementById("means2").innerText = "Ready to move";
+
+// }else{
+// document.getElementById("means2").innerText = "Undefine";
+// }
+
+// console.log("Q no # 3 -- C")
+
+// var color3 = prompt("Enter Traffic lights");
+// document.getElementById("color3").innerText = color3;
+
+// if(color3 === "green"){
+//     document.getElementById("means3").innerText = "Move now";
+
+// }else{
+// document.getElementById("means3").innerText = "Undefine";
+// }
+
+var a = 4;
+if (++a === 5){
+alert("given condition for variable a is true");
+}

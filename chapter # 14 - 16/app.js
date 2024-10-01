a849fcec5ac43bda519ed88195c15f872b116be8
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-02 01:05:56 +0500

    practise

--- chapter # 14 - 16/app.js
+++ chapter # 14 - 16/app.js
@@ -1,26 +1,69 @@
+// Q no 1 *********************************************************************************
 
 var student = [];
+
+// Q no 2 *********************************************************************************
 
 var studentName = [ "Ali", "Ahmed", "Zain"];
 
 console.log(studentName);
 
+// Q no 3 *********************************************************************************
+
 var num = [ 1, 2, 3 ];
 
 console.log(num);
+
+// Q no 4 *********************************************************************************
 
 var boolean = [ true, false ];
 
 console.log(boolean);
 
+// Q no 5 *********************************************************************************
+
 var mixArr = ["Ball", 23, false];
 
 console.log(mixArr);
 
-student = student.push( "Micheal", "Jphn", "tony");
+// Q no 6 *********************************************************************************
 
-console.log(student);
+var qualifications = ["ssc", "hsc", "bcs", "bs", "b-com", "ms", "ph-d"];
 
-var score = ["Micheal's Score in 320 And Got 64%"]
+console.log(qualifications);
 
-document.write(score);
+// Q no 7 *********************************************************************************
+
+var score = [400, 380, 200];
+
+var totalMarks = 500;
+
+//               Ali
+
+var aliScore = score[0];
+
+// console.log(aliScore)
+
+var aliPercentage = ((aliScore / totalMarks) * 100);
+
+// console.log(aliPercentage);
+
+console.log(studentName[0] + "'s Score is :" + aliScore + " and Persentage is :" + aliPercentage + "%");
+
+//              Ahmed
+
+var ahmedScore = score[1];
+
+var ahmedPercentage = ((ahmedScore / totalMarks) * 100);
+
+console.log(studentName[1] + "'s Score is :" + ahmedScore + " and Persentage is :" + ahmedPercentage + "%");
+
+
+//              Zain
+
+var zainScore = score[2];
+var zainPercentage = ((zainScore / totalMarks) * 100);
+
+console.log(studentName[2] + "'s Score is :" + zainScore + " and Persentage is :" + zainPercentage + "%");
+
+// Q no 

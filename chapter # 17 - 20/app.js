f2e1e0f1f7ebe973dfae5a3bdca9e533cfa4f917
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-14 14:56:42 +0500

    ??

--- chapter # 17 - 20/app.js
+++ chapter # 17 - 20/app.js
@@ -1,11 +1,15 @@
 
 // Q no 1
 
+console.log("*****Q no 1*****");
+
 var iniArr = [[], []]
 
 console.log(iniArr);
 
 // Q no 2
+
+console.log("*****Q no 2*****");
 
 var multiArr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
 
@@ -13,20 +17,26 @@ console.log(multiArr);
 
 // Q no 3
 
+console.log("*****Q no 3*****");
+
 for (var i = 0; i <= 9; i++) {
     console.log(i + 1)
 }
 
 // Q no 4
 
-var num = prompt("table number");
-var length = prompt("Enter Table Lenght");
+console.log("*****Q no 4*****");
 
-for (var i = 0; i <= length; i++) {
-    console.log(num + "X" + i + "=" + num * i);
-}
+// var num = prompt("table number");
+// var length = prompt("Enter Table Lenght");
+
+// for (var i = 1; i <= length; i++) {
+//     console.log(num + "X" + i + "=" + num * i);
+// }
 
 // Q no 5
+
+console.log("*****Q no 5*****");
 
 var fruit = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
 
@@ -36,3 +46,62 @@ for(var i = 0; i <= fruit.length - 1; i++){
 
 // Q no 6
 
+console.log("*****Q no 6 [a]*****");  // Counting
+
+for(var i = 1; i <= 15; i++){
+    console.log(i);
+}
+
+console.log("*****Q no 6 [b]*****"); // Reverce Counting
+
+for (var i = 15; i >= 1; i--){
+    console.log(i);
+}
+
+console.log("*****Q no 6 [c]*****"); // Even Counting
+
+for(var i = 0; i <= 20; i += 2){
+    console.log(i);
+}
+
+console.log("*****Q no 6 [d]*****"); // Odd Counting
+
+for(var i = 1; i <= 20; i += 2){
+    console.log(i);
+}
+
+console.log("*****Q no 6 [d]*****"); // Series Counting
+
+for(var i = 2; i <= 20; i += 2){
+    console.log(i + "k");
+}
+
+
+// Q no 7
+
+console.log("***** Q no 7 *****");
+
+// ************************************* ?????? ****************
+
+var user = "cake"; 
+var A = ["cake", "apple pie", "cookie", "chips", "patties"];
+
+for (var i = 0; i < A.length; i++){
+    if(A[i] == user){
+        console.log(user + ": is available");
+    }
+    else if(A[i] != user){
+        console.log(user + ": is available");
+    }
+    // else{
+    //     console.log(user + ": is not available");
+    // }
+}
+
+// Q no 10
+
+console.log("***** Q no 10 *****");
+
+for(var i = 5; i <= 100; i += 5){
+    console.log(i);
+}

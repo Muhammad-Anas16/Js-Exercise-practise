194baa3b46c05a3020b93d4d108fc1ed90c02884
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-14 10:56:13 +0500

    abc

--- chapter # 17 - 20/app.js
+++ chapter # 17 - 20/app.js
@@ -1,3 +1,38 @@
+
+// Q no 1
+
+var iniArr = [[], []]
+
+console.log(iniArr);
+
+// Q no 2
+
 var multiArr = [[0, 1, 2, 3], [1, 0, 1, 2], [2, 1, 0, 1]];
 
 console.log(multiArr);
+
+// Q no 3
+
+for (var i = 0; i <= 9; i++) {
+    console.log(i + 1)
+}
+
+// Q no 4
+
+var num = prompt("table number");
+var length = prompt("Enter Table Lenght");
+
+for (var i = 0; i <= length; i++) {
+    console.log(num + "X" + i + "=" + num * i);
+}
+
+// Q no 5
+
+var fruit = ['apple', 'banana', 'mango', 'orange', 'strawberry'];
+
+for(var i = 0; i <= fruit.length - 1; i++){
+    console.log(fruit[i]);
+}
+
+// Q no 6
+

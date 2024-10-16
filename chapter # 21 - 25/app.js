237db32b8cc2046e57143ba0c759da9096b621bb
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-16 19:04:28 +0500

    ...

---
+++ chapter # 21 - 25/app.js
@@ -0,0 +1,24 @@
+// Q no 1
+
+console.log("***** Q no 1 *****");
+
+// var firstName = prompt("Enter your first name");
+// var lastName = prompt("Enter your last name");
+
+// var fullName = firstName + " " + lastName;
+
+// console.log(fullName);
+
+// Q no 2
+
+console.log("***** Q no 2 *****");
+
+// var user = prompt("Enter your favorite mobile phone model");
+
+// console.log("My favorite mobile is :" + user + " : and it's lenght of string is :" + user.length);;
+
+// Q no 3
+
+console.log("***** Q no 3 *****");
+
+var str = "pakistan"

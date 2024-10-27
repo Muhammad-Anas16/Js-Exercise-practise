df3d1bbd0e4e0e94278507ca24b0df3d34e925f9
muhammad-anas16 <anas.hanf38@gmail.com>
2024-10-27 19:49:20 +0500

    today Practise

---
+++ chapter # 31 - 34/app.js
@@ -0,0 +1,50 @@
+// Q no # 1
+console.log('**** Q no # 1 ****');
+
+let date = new Date();
+console.log(date)
+
+// Q no # 2
+console.log('**** Q no # 2 ****');
+
+let monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+let month = date.getMonth();
+
+console.log('The Corrent Month :', monthArr[month]);
+
+// Q no # 3
+console.log('**** Q no # 3 ****');
+
+let dayArr = ["Sun", "Mon", "Tues", "Wednes", "Thurs", "Fri", "Satur"];
+let days = date.getDay()
+
+console.log('The Corrent Day :', dayArr[days]);
+
+// Q no # 4
+console.log('**** Q no # 4 ****');
+
+// let ask = prompt('Enter 0 or 1');
+// let usr = ['saturday', 'sunday'];
+
+// if (usr[ask] == 'saturday' || usr[ask] == 'sunday') {
+//     console.log(`${usr[ask]} is Fun Day`);
+// }
+// else if(usr[ask] == undefined){
+//     console.log('Enter 0 or 1');
+// }
+
+// Q no # 5
+console.log('**** Q no # 5 ****');
+
+// let qno5 = prompt('Enter Number Under 30');
+
+// if(qno5 < 16){
+//     console.log('First fifteen days of the month');
+// }
+// else if(qno5 < 31 ){
+//     console.log('Last fifteen days of the month');
+// }
+
+// Q no # 6
+console.log('**** Q no # 6 ****');
+
